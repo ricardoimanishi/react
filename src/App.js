0db2a52783e92498f60d16122fa2312a87aa5097
Ricardo Imanishi <ti2@ortodonticcenter.com.br>
2018-01-09 23:02:53 -0200

    Adicionando estado a um componente

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id" : "ryan",
-    "name" : "Ryan Florence",
-    "email" : "ryan@reacttraining.com",
-    "avatarURL" : "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id" : "michael",
-    "name" : "michael Jackson",
-    "email" : "michaeljackson@reacttraining.com",
-    "avatarURL" : "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id" : "tyler",
-    "name" : "Tyler McGinnis",
-    "email" : "tyler@reacttraining.com",
-    "avatarURL" : "http://localhost:5001/tyler.jpg"
-  }
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+    {
+      "id" : "ryan",
+      "name" : "Ryan Florence",
+      "email" : "ryan@reacttraining.com",
+      "avatarURL" : "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id" : "michael",
+      "name" : "michael Jackson",
+      "email" : "michaeljackson@reacttraining.com",
+      "avatarURL" : "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id" : "tyler",
+      "name" : "Tyler McGinnis",
+      "email" : "tyler@reacttraining.com",
+      "avatarURL" : "http://localhost:5001/tyler.jpg"
+    }
+  ]
+}
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     )
   }
